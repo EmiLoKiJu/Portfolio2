55d45a2f7d85ff9a474e680ac4f2f06cffbdbf27
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-11-14 14:27:28 -0600

    Fix linter

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
+const __dirname = ''
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
