52ad8074e979e406589d5ca896fa4ab3a3ce35a9
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-11-15 10:45:12 -0600

    Add form

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -27,6 +27,61 @@ const Portfolio = ()=> {
   const [xSrc, setXSrc] = useState(TwitterXGrey);
   const [mediumSrc, setMediumSrc] = useState(MediumGrey);
 
+  const [githubSrc2, setGithubSrc2] = useState(GithubBlack);
+  const [linkedInSrc2, setLinkedInSrc2] = useState(LinkedInBlack);
+  const [angelistSrc2, setAngelistSrc2] = useState(AngelistBlack);
+  const [xSrc2, setXSrc2] = useState(TwitterXBlack);
+  const [mediumSrc2, setMediumSrc2] = useState(MediumBlack);
+
+  const [formData, setFormData] = useState({
+    user_name: '',
+    user_email: '',
+    user_message: '',
+  });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const [submited, setSubmited] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value, // Trim the input value
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    formData.user_name = formData.user_name.trim();
+    formData.user_email = formData.user_email.trim();
+    formData.user_message = formData.user_message.trim();
+    if (Object.values(formData).some((value) => value === '')) {
+      setErrorMessage('Fields cannot be blank or full of spaces');
+      return;
+    }
+
+    setErrorMessage('');
+    try {
+      const response = await fetch('https://formspree.io/f/xzbqjzwo', {
+        method: 'POST',
+        body: JSON.stringify(formData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error('Form submission failed');
+      }
+
+      console.log('Form submitted:', formData);
+      setSubmited(true);
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
     return(
       <div>
         <div className="w-full bg-black2 pt-[165px] px-[8%]">
@@ -109,7 +164,7 @@ const Portfolio = ()=> {
           </ul>
           <button className="text-white bg-orange2 font-inter font-bold p-3">See project</button>
         </div>
-        <div className="flex flex-col w-full bg-black2 pt-[120px] px-[8%] h-[720px]">
+        <div className="flex flex-col w-full bg-black2 mt-[120px] pt-[120px] px-[8%] h-[720px]">
           <h1 className="font-crete text-5xl text-orange2 ">
             About me
           </h1>
@@ -119,7 +174,7 @@ const Portfolio = ()=> {
         </div>
         <div className="flex flex-col w-full bg-black2 pt-[60px] px-[8%] h-[920px]">
           <div className="flex flex-col justify-center items-center">
-            <div className="flex justify-center pr-[10%] my-2">
+            <div className="flex justify-center pr-[8%] my-2">
               <img src={Rec1} alt="O0" className="object-contain w-5 mx-3"></img>
               <h2 className="font-bold text-white font-roboto text-3xl">Language</h2>
             </div>
@@ -127,7 +182,7 @@ const Portfolio = ()=> {
             <h3 className="font-inter text-[22px] text-white my-1 leading-9">HTML</h3>
             <h3 className="font-inter text-[22px] text-white my-1 leading-9">Tailwind CSS</h3>
             <h3 className="font-inter text-[22px] text-white my-1 leading-9">Ruby</h3>
-            <div className="flex justify-center pr-[10%] my-2">
+            <div className="flex justify-center pr-[8%] my-2">
               <img src={Rec2} alt="O0" className="object-contain w-4 mx-3"></img>
               <h2 className="font-bold text-white font-roboto text-3xl">Frameworks</h2>
             </div>
@@ -135,7 +190,7 @@ const Portfolio = ()=> {
             <h3 className="font-inter text-[22px] text-white my-1 leading-9">Ruby on Rails</h3>
             <h3 className="font-inter text-[22px] text-white my-1 leading-9">RSpec</h3>
             <h3 className="font-inter text-[22px] text-white my-1 leading-9">Capybara</h3>
-            <div className="flex justify-center pr-[10%] my-2">
+            <div className="flex justify-center pr-[8%] my-2">
               <img src={Rec3} alt="O0" className="object-contain w-4 mx-3"></img>
               <h2 className="font-bold text-white font-roboto text-3xl">Skills</h2>
             </div>
@@ -144,6 +199,82 @@ const Portfolio = ()=> {
             <h3 className="font-inter text-[22px] text-white my-1 leading-9">API design</h3>
             <h3 className="font-inter text-[22px] text-white my-1 leading-9">Web development</h3>
           </div>
+        </div>
+        <div className="px-[8%] mt-16">
+          <h2 className="text-center font-inter text-3xl font-bold"> I'm always interested in hearing about new projects. If you'd like to contact me, get in touch so we can talk about new project oportunities</h2>
+          {submited ? (<div className="roboto text-2xl text-center my-8">Thank you for your time!</div>) : (<div className="px-[8%] mt-16">
+            <form onSubmit={handleSubmit} className="flex flex-col" method="post">
+              <input
+                className="pl-4 mt-16 font-inter"
+                type="text"
+                id="name"
+                name="user_name"
+                placeholder="Full name"
+                value={formData.user_name}
+                onChange={handleChange}
+              />
+              <div className="border-t border-black-400 mt-4 mb-8"></div>
+              <input
+                className="pl-4 font-inter"
+                id="mail"
+                name="user_email"
+                placeholder="Email address"
+                value={formData.user_email}
+                onChange={handleChange}
+              />
+              <small></small>
+              <div className="border-t border-black-400 mt-4 mb-8"></div>
+              <textarea
+                className="pl-4 font-inter"
+                id="msg"
+                name="user_message"
+                placeholder="Write me something..."
+                value={formData.user_message}
+                onChange={handleChange}
+              ></textarea>
+              {errorMessage && <p className="text-red-500 font-inter pl-4">{errorMessage}</p>}
+              <button className="mx-auto text-white bg-orange2 font-inter font-bold p-3 mb-[70px] mt-8 w-[160px]" type="submit">
+                Get in touch
+              </button>
+            </form>
+          </div>)}
+        </div>
+        <div className="mx-auto flex justify-center pt-4 pb-[165px]">
+          <img
+            className="w-7 p-[5px] object-contain m-2"
+            src={githubSrc2}
+            alt="Github Logo"
+            onMouseEnter={() => setGithubSrc2(GithubGrey)}
+            onMouseLeave={() => setGithubSrc2(GithubBlack)}
+          />
+          <img
+            className="w-7 p-1 object-contain m-2"
+            src={linkedInSrc2}
+            alt="LinkedIn Logo"
+            onMouseEnter={() => setLinkedInSrc2(LinkedInGrey)}
+            onMouseLeave={() => setLinkedInSrc2(LinkedInBlack)}
+          />
+          <img
+            className="w-7 p-1 object-contain m-2"
+            src={angelistSrc2}
+            alt="Angelist Logo"
+            onMouseEnter={() => setAngelistSrc2(AngelistGrey)}
+            onMouseLeave={() => setAngelistSrc2(AngelistBlack)}
+          />
+          <img
+            className="w-7 object-contain m-2"
+            src={xSrc2}
+            alt="X Logo"
+            onMouseEnter={() => setXSrc2(TwitterXGrey)}
+            onMouseLeave={() => setXSrc2(TwitterXBlack)}
+          />
+          <img
+            className="w-7 p-1 object-contain m-2"
+            src={mediumSrc2}
+            alt="Medium Logo"
+            onMouseEnter={() => setMediumSrc2(MediumGrey)}
+            onMouseLeave={() => setMediumSrc2(MediumBlack)}
+          />
         </div>
       </div>
     );
