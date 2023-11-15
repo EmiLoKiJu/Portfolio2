92de27b9e2235e963494c9ec71f3116d81a50e1e
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-11-15 13:37:21 -0600

    Add logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,15 @@
+import Logo from '@/assets/Logosmall.png'
+
 const Nav = ()=> {
   return(
-    <nav className="bg-grey2 fixed w-full text-white">This is the nav</nav>
+    <nav className="bg-grey2 fixed w-full text-white">
+      <img
+        className="w-12 p-[5px] object-contain"
+        src={Logo}
+        alt="Github Logo"
+      >
+      </img>
+    </nav>
   );
 }
 
