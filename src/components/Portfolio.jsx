1849540557baa4d498fa3e1bdc74525d547c0ba3
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-11-28 15:06:18 -0600

    Add link to resume

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -377,7 +377,7 @@ const Portfolio = ()=> {
           About me
         </h1>
         <p className="obs transition-colors ease-in-out delay-100 duration-[1000ms] text-base leading-6 font-inter text-white my-6 max-w-[300px] ">I&apos;m a person who discovered his passion to program when I was studing electronic engineering between 2014 and 2018, and after I left, there was a gap where I tried learning how to program videogames, and after that I joined the program to become a Full-Stack Software Developer. <br/> The best atributes you will find in myself are responsability, logical-thinking, problem-solving skills, and adaptability to learn new technologies.</p>
-        <button className="text-white bg-orange2 font-inter font-bold p-3 mb-[220px] mt-auto w-[180px]">Get my resume</button>
+        <a className="flex mb-[220px] mt-auto" href="https://docs.google.com/document/d/1FqKTVkPu6RHxVKqzN7IfH15wnpKxpsko43HQjIANwGM/edit?usp=sharing" target="_blank"><button className="text-white bg-orange2 font-inter font-bold p-3 w-[180px]">Get my resume</button></a>
         <div className="border-t border-white-400 mb-16"></div>
       </div>
       <div className="flex flex-col w-full bg-black2 pt-[60px] px-[8%] h-[920px] lg:h-[520px]">
